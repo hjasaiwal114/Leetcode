00b243c21a5b298e73c52f1cb9efec95ad3592f4
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-04-25 19:37:16 +0530

    solve 25th april question

---
+++ leetcode 2336.py
@@ -0,0 +1,13 @@
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.nums = list(range(1, 1001))
+
+    def popSmallest(self) -> int:
+        self.nums.sort()
+        n = self.nums[0]
+        self.nums[:] = self.nums[1:]
+        return n
+    def addBack(self, num: int) -> None:
+        if num not in self.nums:
+            self.nums.append(num)
