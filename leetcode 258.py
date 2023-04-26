61c9ee1c1c1ed11016f8eadb0ccb1e5f52b2ee34
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-04-26 20:50:24 +0530

    more optimal solution with in built formula

--- leetcode 258.py
+++ leetcode 258.py
@@ -1,8 +1,16 @@
-def add_digits(num):
-    while num > 9:
-        digit_sum = 0
-        while num > 0:
-            digit_sum += num % 10
-            num //= 10
-        num = digit_sum
-    return num
+class less_Optimal():
+    def add_digits(num):
+        while num > 9:
+            digit_sum = 0
+            while num > 0:
+                digit_sum += num % 10
+                num //= 10
+            num = digit_sum
+        return num
+class more_optimal():
+    def add_digits(num):
+    if num == 0:
+        return 0
+    return 1 + ((num - 1) % 9)
+# formula (num - 1) % 9
+
