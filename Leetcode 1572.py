4184eca7a3180d5b792585fe1edc05d695e74a49
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-05-08 23:53:01 +0530

    solved 8 amay problem

---
+++ Leetcode 1572.py
@@ -0,0 +1,8 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        res, n = 0, len(mat)
+        for i in range(len(mat)):
+            res += mat[i][i] # primary diagonal 
+            res += mat[i][len(mat) - 1 - i] # secondary diagonal
+
+        return res -( mat[n //2][n // 2] if n%2 else 0 )
