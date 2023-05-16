a088fee1ce752e915f5ce8a2c1b6f0998acbc4dd
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-05-16 23:43:06 +0530

    16 ay challange

--- Leetcode 24.py
+++ Leetcode 24.py
@@ -1,3 +1,23 @@
 class Solution:
     def swapPairs(sef, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
         
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prev = dummy
+        curr = head
+
+        while curr and curr.next:
+            first_node = curr
+            second_node = curr.next
+
+            prev.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            prev = first_node
+            curr = first_node.next
+        
+        return dummy.next
