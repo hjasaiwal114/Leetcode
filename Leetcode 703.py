a368113796188e455183094e490538be20dcf77e
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-05-23 23:46:23 +0530

    solving 23 May

--- Leetcode 703.py
+++ Leetcode 703.py
@@ -1,2 +1,13 @@
 import heapq
 
+class KthLargest:
+    def __init__(self, k, nums):
+        self.k = K
+        self.heap = []
+        for num in nums:
+            self.add(num)
+    def add(self, val):
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return sel.heaap[0]
