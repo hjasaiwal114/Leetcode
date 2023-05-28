5c197303062fa7a76bf43de9d0dc0afe7389bd9c
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-05-28 18:11:44 +0530

    solve 28 may

--- Leetcode 1547.py
+++ Leetcode 1547.py
@@ -1,4 +1,23 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
+        dp = {}
 
-        def
+        def dfs(l, r):
+            if r - l == 1:
+                return 0
+            if (l, r) in dp:
+                return dp[(l, r)]
+
+            res = float("inf")
+            for c in cuts:
+                if l < c < r:
+                    res = min(
+                        res,
+                        (r-1) + dfs(l, c) + dfs(c, r)
+                    )
+
+            dp[(l, r)] = res = 0 if res == float("inf") else res
+            return res  
+
+        
+        return dfs(0, n)
