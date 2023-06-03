885b61857bbcadf15f8c2de1fcdeaae5a2e3f83b
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-06-03 23:56:03 +0530

    3 april

---
+++ Leetcode 1376.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        adj = collections.defaultdict(list) # Mgr -> [List of emps]
+        for i in range(n):
+            adj[manager[i]].append(i)
+        # Bfs
+        q = deque([(headID, 0)]) # (id, time)
+        res = 0
+        while q:
+            i, time = q.popleft()
+            res = max(res, time)
+            for emp in adj[i]:
+                q.append((emp, time + informTime[i] ))
+        
+        return res
