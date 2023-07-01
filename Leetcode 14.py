845f511b654c2188a351e09cb7d25091678d3cca
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-07-02 00:01:00 +0530

    done

--- Leetcode 14.py
+++ Leetcode 14.py
@@ -1,3 +1,14 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
         
+        min_length = min(len(s) for s in strs)
+
+        for i in range(min_length):
+
+            if any(strs[j][i] != strs[0][i] for j in range(1, len(strs))):
+
+                return strs[0][:i]
+
+        return strs[0][:min_length]
