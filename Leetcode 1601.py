2bec9cdeb6e3ec98572dd6065c4493933feccf5f
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-07-02 23:37:18 +0530

    solved

--- Leetcode 1601.py
+++ Leetcode 1601.py
@@ -1,2 +1,25 @@
 class Solution:
-    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+    def __init__(self):
+        self.ans = 0
+    
+    def helper(self, start, requests, indegree, n, count):
+        if start  == len(requests):
+            for i in range(n):
+                if indegree[i] != 0:
+                    return
+            self.ans = max(self.ans, count)
+            return
+        # Take
+        indegree[requests[start][0]] -= 1
+        indegree[requests[start][1]] += 1
+        self.helper(start + 1, requests, indegree, n, count + 1)
+
+        # Non-Take
+        indegree[requests[start][0]] += 1
+        indegree[requests[start][1]] -= 1
+        self.helper(start + 1, requests, indegree, n, count)
+
+    def maximumRequests(self, n, requests):
+        indegree = [0] * n
+        self.helper(0, requests, indegree, n, 0)
+        return self.ans
