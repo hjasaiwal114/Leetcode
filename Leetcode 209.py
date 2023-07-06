1deafc78902fc346f046628c9c6cd89c62dc258d
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-07-06 23:53:11 +0530

    7 july

--- Leetcode 209.py
+++ Leetcode 209.py
@@ -1,3 +1,14 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        
+        l = 0
+        res = float('inf')
+        s = 0
+
+        for r in range(len(nums)):
+            s += nums[r]
+
+            while s >= target:
+                res = min(res, r - l + 1)
+                s -= nums[l]
+                l += 1
+        return res if res != float('inf') else 0
