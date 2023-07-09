f6f2b8db460c34f8f889282e88559a0ce30e1528
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-07-09 23:08:38 +0530

    question

---
+++ Leetcode 2272.py
@@ -0,0 +1,12 @@
+"""
+2272. Substring With Largest Variance
+
+The variance of a string is defined as the largest diffrence
+between the number of occurrences of any 2 characters present in the string.
+Note the two characters may or may not be the same.
+
+given a string s consisting of lowercase English letter only, return
+the largest varience possible among all substring of s.
+
+a substring is a contiguous sequence is characters whithin a string.
+"""
