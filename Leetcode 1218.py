4f468a4558d3741a072a4057d9c7d7ac038ffe7d
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-07-14 23:54:33 +0530

    solved 14 july

--- Leetcode 1218.py
+++ Leetcode 1218.py
@@ -4,3 +4,18 @@ Given an integer array arr and an integer difference, return the length of the l
 
 A subsequence is a sequence that can be derived from arr by deleting some or no elements without changing the order of the remaining elements.
 """
+
+from collections import defaultdict
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], dif: int) -> int:
+        # Create a defaultdict to store the lengths of subsequences
+        lengths = defaultdict(lambda: 0)
+        
+        # Iterate over the array elements
+        for num in arr:
+            # Calculate the length of the subsequence ending at the current element
+            lengths[num] = lengths[num - dif] + 1
+
+        # Return the maximum length of subsequences
+        return max(lengths.values())
