47fb8bef58fd9adaba57be224c998d27b75d7fa0
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-07-19 23:52:07 +0530

    adiing function

--- Leetcode 435.py
+++ Leetcode 435.py
@@ -3,3 +3,6 @@
 Given an array of intervals where intervals[i] = [start, end] , retrn the minimum number of intervals
 you need to remove to make the rest of the intervals non-overlapping.
 """
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        
